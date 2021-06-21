b1ca5139b0340f32723645b941aea923764bbe40
joseph <joseph.m.valdez1@gmail.com>
2021-06-21 08:51:47 -0700

    Created Main client, Game class, and added Prompter util

---
+++ src/com/dealornodeal/client/Main.java
@@ -0,0 +1,11 @@
+package com.dealornodeal.client;
+
+import com.dealornodeal.Game;
+
+class Main {
+    public static void main(String[] args) {
+
+        Game game = new Game("Deal or No Deal");
+        game.startGame();
+    }
+}

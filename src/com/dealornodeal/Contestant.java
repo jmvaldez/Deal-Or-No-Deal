aca3977df35e9241805b468d0dd85f7005e0f7b2
Jauric Flowers <jflow415@gmail.com>
2021-06-22 14:16:08 -0400

    changes to contestant class

--- src/com/dealornodeal/Contestant.java
+++ src/com/dealornodeal/Contestant.java
@@ -1,9 +1,15 @@
 package com.dealornodeal;
 
+import com.apps.util.Prompter;
+
+import java.util.Scanner;
+
 public class Contestant {
     //Fields
     private String contestantName;
     Briefcase briefcase = new Briefcase();
+    int round = 1;
+    length =bank.availableCases.size()
 
     //Ctor
     public Contestant() {
@@ -33,6 +39,40 @@ public class Contestant {
     }
 
     public void chooseBriefCase() {
-        System.out.println(contestantName + "chooses briefcase number " + briefcase.getCaseNumber());
+        //System.out.println(contestantName + "chooses briefcase number " + briefcase.getCaseNumber());
+        //Game game = new Game();
+
+        switch (round)
+        {
+            case 1 : length = 25;
+                break;
+            case 2 : length = 19;
+                break;
+            case 3 : length  = 14;
+                break;
+            case 4 : length = 10;
+                break;
+            case 5 : length = 7;
+                break;
+            case 6 : length = 6;
+            break;
+            case 6 : length = 5;
+                break;
+            case 6 : length = 4;
+                break;
+            case 6 : length = 3;
+                break;
+            case 6 : length = 2;
+                break;
+        }
+
+        
+
+
+//        public Round nextRound() {
+//            return this.ordinal() < Round.values().length - 1
+//                    ? Round.values()[this.ordinal() + 1]
+//                    : null;
+//        }
     }
 }
